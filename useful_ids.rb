e60706c4a19d545950f1b62e5ec14a51d0232487
bulgakke <vvp835@yandex.ru>
2020-03-16 22:28:18 +0300

    fixed Tools.get_id

--- useful_ids.rb
+++ useful_ids.rb
@@ -5,15 +5,16 @@ module Tools
 
   def self.get_id(bot, message)
     text = "$$$$$"
-    bot.respond_to_user(message, text)
+    bot.send_message(chat_id: message.chat.id, text: text)
     bot.listen do |response|
       if response.text.starts_with?('$$$$$')
         text = "module Tools
 BOT_ID = #{response.from.id}
 OWNER_ID = #{message.from.id}
 end"
-        bot.respond_to_user(message, text)
+        bot.send_message(chat_id: message.chat.id, text: text)
         text = "Copy-paste this text into your project"
+        bot.send_message(chat_id: message.chat.id, text: text)
       end
     end
   end
