696ed15c3e5393b2d11ca90d78c2f1742b8bb45e
bulgakke <vvp835@yandex.ru>
2020-03-16 23:05:57 +0300

    pulled the #command out of String class to the Message class

--- example_main.rb
+++ example_main.rb
@@ -9,7 +9,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     puts 'Received a message'
 
-    case message.text.command
+    case message.command
     when '/start'
       text = 'Greeting, hooman!'
       bot.api.send_message(chat_id: message.chat.id, text: text)
