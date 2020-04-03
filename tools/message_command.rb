cfe18b83388e520e1e0742ba0986d2a0367adb24
bulgakke <vvp835@yandex.ru>
2020-04-03 21:35:37 +0300

    Fix command detection for messages without text

--- tools/message_command.rb
+++ tools/message_command.rb
@@ -3,6 +3,7 @@ module Telegram
     module Types
       class Message
         def command
+          return '' unless text
           text.split[0].delete_suffix(Tools::BOT_USERNAME)
         end
       end
